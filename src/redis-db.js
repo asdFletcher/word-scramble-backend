14cde4b103b9fe9b801e9075798a88b44b206085
Fletcher LaRue <laruefletcher@gmail.com>
2019-07-08 15:50:23 -0700

    debugging deployed redis

--- src/redis-db.js
+++ src/redis-db.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const asyncRedis = require("async-redis");
-const client = asyncRedis.createClient();
+const client = asyncRedis.createClient(process.env.REDIS_URL);
 
 client.on('connect', () => {
   console.log(`Redis client connected`);
